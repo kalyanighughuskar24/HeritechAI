301ca418edf3cb9378f3beb8bf5125be1aed6a3a
Kalyani Ghughuskar <163885025+kalyanighughuskar24@users.noreply.github.com>
2026-07-04 23:51:07 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "ai_community");
+$conn = new mysqli("sql102.infinityfree.com", "if0_42291565", "4yR5Jqix6CVHh", "if0_42291565_ai_community");
 
 if($conn->connect_error){
     die("Connection Failed: ".$conn->connect_error);
 }
-?>
+?> 
